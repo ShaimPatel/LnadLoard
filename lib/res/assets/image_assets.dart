3cb0034542a125215bd2a9058e810c8f557dfd05
Shivam Patel <sp5036018@gmail.com>
2023-12-01 12:26:44 +0530

    Adding Image into a custome file

---
+++ lib/res/assets/image_assets.dart
@@ -0,0 +1,63 @@
+class ImageAssets {
+  static const String personImage = 'images/person.png';
+  static const String person2Image = 'images/person2.png';
+  static const String dots = 'images/dots.png';
+  static const String favicon = 'images/favicon.png';
+  static const String smartlink = 'images/smartlink.png';
+  static const String userAvatar = 'images/User-avatar.svg.png';
+  static const String user1 = 'images/user1.png';
+  static const String user2 = 'images/user2.png';
+  static const String user3 = 'images/user3.png';
+
+  static const String errorGif = 'images/Error.gif';
+  static const String loader = 'images/loader.gif';
+  static const String loader1 = 'images/loader1.gif';
+
+  static const String commercialComp = 'images/COMMERCIAL-comp.jpg';
+  static const String commercial = 'images/COMMERCIAL.jpg';
+  static const String contact = 'images/contact.jpg';
+  static const String development = 'images/DEVELOPMENT.jpg';
+
+  static const String heroBg1 = 'images/hero_bg_1.jpg';
+  static const String heroBg2 = 'images/hero_bg_2.jpg';
+  static const String heroBg3 = 'images/hero_bg_3.jpg';
+
+  static const String home1 = 'images/HOME-1.jpg';
+  static const String home2 = 'images/HOME-2.jpg';
+  static const String home3 = 'images/HOME-3.jpg';
+
+  static const String img1 = 'images/img_1.jpg';
+  static const String img2 = 'images/img_2.jpg';
+  static const String img3 = 'images/img_3.jpg';
+  static const String img4 = 'images/img_4.jpg';
+  static const String img5 = 'images/img_5.jpg';
+  static const String img6 = 'images/img_6.jpg';
+  static const String img7 = 'images/img_7.jpg';
+  static const String img18 = 'images/img_8.jpg';
+
+  static const String letting = 'images/LETTING.jpg';
+  static const String manAvater = 'images/man-avatar.jpg';
+  static const String sales = 'images/SALES.jpg';
+  static const String smartlinkAboutus = 'images/smartlink-aboutus.jpg';
+  static const String services = 'images/services.jpg';
+  static const String valuattion = 'images/VALUATION.jpg';
+
+  static const String welcomeSmartLink = 'images/welcome-smart-link.svg';
+
+  //! CompanyLogos
+  static const String clientMoney = 'images/companyLogo/clientMoney.png';
+  static const String dps = 'images/companyLogo/dps.png';
+  static const String findaProperty = 'images/companyLogo/finda-property.png';
+  static const String globrix = 'images/companyLogo/globrix.png';
+  static const String gumtree = 'images/companyLogo/gumtree.png';
+  static const String lcoLogo = 'images/companyLogo/IcoLogo.png';
+  static const String onTheMarket = 'images/companyLogo/onTheMarket.png';
+  static const String prs = 'images/companyLogo/prs.png';
+  static const String rla = 'images/companyLogo/rla.png';
+  static const String xp = 'images/companyLogo/xp.png';
+  static const String rzoopla = 'images/companyLogo/zoopla.png';
+
+  static const String technosysinfowareltd =
+      'images/companyLogo/technosysinfowareltd.jpg';
+  static const String house = 'images/companyLogo/house.jpg';
+}
---
+++ lib/res/colors/app_color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color blackColor = Color(0xff000000);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color primaryColor = Color(0xff6157F1);
+  static const Color backColor = Color(0xff4682E2);
+  static const Color homeCardsColor = Color(0xff8881F4);
+  static const Color forgrButtonColor = Color(0xff424242);
+  static const Color textfieldcolor = Color(0xffFAFAFA);
+}
